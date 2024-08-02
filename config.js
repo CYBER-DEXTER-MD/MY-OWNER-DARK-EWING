c692700d35cb582cf57a12ddfe8c6a1416749f4f
⚔ DEXTER ⚔ <143838400+DEXTER-BOTS@users.noreply.github.com>
2024-08-03 00:55:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || 'zokk',
+module.exports = { sessionName: process.env.SESSION_ID || 'zokk',
     PREFIXE: process.env.PREFIX || ".",
     OWNER_NAME: process.env.OWNER_NAME || "⚔  dexter  ⚔",
     NUMERO_OWNER : process.env.OWNER_NUMBER || "",              
